e4b0b68cad188b4f8ac4805d0c17c77ac552e9b2
Danila-F <danilaphiliptsow@gmail.com>
2025-03-20 11:07:26 +0300

    Remove TestClass.java

--- classes/TestClass.java
+++
@@ -1,5 +0,0 @@
-public class TestClass {
-    public void test() {
-        System.out.println("Method test() of class TestClass is running...");
-    }
-}
--- src/com/reservationapp/TestClass.java
+++
@@ -1,7 +0,0 @@
-package com.reservationapp;
-
-public class TestClass {
-    public void test() {
-        System.out.println("Method test() of class TestClass is running...");
-    }
-}
